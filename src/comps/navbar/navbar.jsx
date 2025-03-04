ef84e5db8a50a70295854854253e6453345c85b9
Lars <laarrss@protonmail.com>
2025-03-04 13:23:37 +0100

    mobile navigation

--- src/comps/navbar/navbar.jsx
+++ src/comps/navbar/navbar.jsx
@@ -1,11 +1,26 @@
 import styles from "./navbar.module.css";
 import { useRef, useState, useEffect } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { GrClose } from "react-icons/gr";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function navbar() {
   const nav = useRef();
   const [showing, setShowing] = useState(false);
 
+  const [showMobile, setShowMobile] = useState(false);
+
+  const handleNavTurn = () => {
+    if (showMobile) {
+      setShowMobile(false);
+    } else {
+      setShowMobile(true);
+    }
+  };
+
   useEffect(() => {
+    setShowMobile(false);
+
     const handleScroll = () => {
       console.log("scroll");
 
@@ -25,13 +40,18 @@ export default function navbar() {
 
   return (
     <>
-      <nav ref={nav} className={`${showing ? styles.showColors : ""}`}>
+      <nav
+        ref={nav}
+        className={`${showing ? styles.showColors : ""} ${
+          showMobile ? styles.mobile : ""
+        }`}
+      >
         <div className="container">
           <div className={styles.navCon}>
             <div class={styles.logo}>
               <h2>Logo</h2>
             </div>
-            <ul>
+            <ul onClick={handleNavTurn}>
               <li>
                 <a href="#">sec1</a>
               </li>
@@ -53,6 +73,9 @@ export default function navbar() {
                 </a>
               </li>
             </ul>
+            <div className={styles.hamburgerCon} onClick={handleNavTurn}>
+              {showMobile ? <GrClose /> : <GiHamburgerMenu />}
+            </div>
           </div>
         </div>
       </nav>
